db244c537ed0d204a366f15ffbb69ab21af74060
osafumi yoshida <moon29.5@icloud.com>
2016-09-05 16:53:02 +0900

    Initial Commit

---
+++ Ipon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ipon
+//
+//  Created by Osafumi Yoshida on 2016/09/05.
+//  Copyright © 2016年 Osafumi Yoshida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

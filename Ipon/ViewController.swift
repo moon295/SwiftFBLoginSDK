1fe58a7d14ddcaac15a63f50f927f9cbea9769ec
osafumi yoshida <moon29.5@icloud.com>
2016-09-07 20:45:02 +0900

    FBLogin実装

--- Ipon/ViewController.swift
+++ Ipon/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let loginView : FBSDKLoginButton = FBSDKLoginButton()
+        self.view.addSubview(loginView)
+        loginView.center = self.view.center
+        loginView.readPermissions = ["public_profile", "email"]
+//        loginView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //ログインボタンが押された時の処理。Facebookの認証とその結果を取得する
+    func loginButton(loginButton: FBSDKLoginButton!,didCompleteWithResult
+        result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        
+        if ((error) != nil)
+        {
+            //エラー処理
+        } else if result.isCancelled {
+            //キャンセルされた時
+        } else {
+            //必要な情報が取れていることを確認(今回はemail必須)
+            if result.grantedPermissions.contains("email")
+            {
+                // 次の画面に遷移（後で）
+            }
+        }
+    }
 }
 
